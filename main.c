1b05a1a28cd03ed44d60fa9fa057476529fe24da
evanengelhardt <eaengelh@gmail.com>
2018-05-05 00:01:58 -0500

    Implements mergesort and makes a function specifically for getting in the array information from the user

--- main.c
+++ main.c
@@ -1,49 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void getArray();
 void bubbleSort(int * pArray, int howMany);
 void quickSort(int * pArray, int start, int end);
+void mergeSort(int* pArray, int length);
+void mergeSortHelp(int * pArray, int * pTempArray, int start, int end);
+void merge(int * pArray, int * pTempArray, int leftStart, int mid, int rightEnd);
 void swap(int *x, int *y);
+
+int i, howMany;
+int * pArrayToSort;
+char typeOfSort;
+
 
 int main()
 {
-    int i, howMany;
-    int * pArrayToSort;
-    char typeOfSort;
 
-    printf("How many numbers would you like to sort?\n");
-    scanf(" %d", &howMany);
-
-    pArrayToSort = (int *) malloc(howMany * sizeof(int));
-
-    printf("Enter each number followed by clicking enter\n");
-
-    for(i = 0 ; i < howMany ; i++){
-        scanf(" %d", &pArrayToSort[i]);
-    }
-
-    printf("here are your unsorted numbers numbers\n");
-
-    for(i = 0 ; i < howMany ; i++){
-        printf("%d ", *(pArrayToSort + i));
-    }
-
-    printf("\nWhat type of sort would you like to do?\nType b for bubblesort\nType q for quicksort\nType m for mergesort\n");
-    scanf(" %c", &typeOfSort);
-
-    printf("\nYou chose the %c sort\n", typeOfSort);
+    getArray();
 
     switch(typeOfSort){
         case 'b' :
+            printf("Using bubblesort\n");
             bubbleSort(pArrayToSort, howMany);
             break;
         case 'q' :
+            printf("Using quicksort\n");
             quickSort(pArrayToSort, 0, howMany - 1);
+            break;
+        case 'm' :
+            printf("Using mergesort\n");
+            mergeSort(pArrayToSort, howMany);
             break;
         default:
             printf("No Correct Sort Chosen\n");
     }
-
 
     printf("Sorted Array\n");
 
@@ -55,14 +46,37 @@ int main()
     return 0;
 }
 
-void swap(int *x, int *y){
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-    return;
+/*
+getArray
+communicates with the user and takes the input of
+array size, array contents, and type of sorting to proceed with
+*/
+void getArray(){
+    printf("How many numbers would you like to sort?\n");
+    scanf(" %d", &howMany);
+
+    pArrayToSort = (int *) malloc(howMany * sizeof(int));
+
+    printf("\nEnter each number followed by clicking enter\n");
+    for(i = 0 ; i < howMany ; i++){
+        scanf(" %d", &pArrayToSort[i]);
+    }
+
+    printf("\n\nHere are your unsorted numbers numbers\n");
+    for(i = 0 ; i < howMany ; i++){
+        printf("%d ", *(pArrayToSort + i));
+    }
+
+    printf("\n\nWhat type of sort would you like to do?\nType b for bubblesort\nType q for quicksort\nType m for mergesort\n");
+    scanf(" %c", &typeOfSort);
+
+    printf("\n\nYou chose the %c sort\n\n", typeOfSort);
 }
 
+/*
+bubbleSort
+sorts the array using the bubbleSort algorithm
+*/
 void bubbleSort(int * pArray, int howMany){
     int i;
     int j;
@@ -76,6 +90,10 @@ void bubbleSort(int * pArray, int howMany){
     return;
 }
 
+/*
+quickSort
+sorts algorithm using the quicksort algorithm
+*/
 void quickSort(int * pArray, int start, int end){
     if(start < end){
         int pivot = *(pArray + end);
@@ -89,7 +107,6 @@ void quickSort(int * pArray, int start, int end){
                 continue;
             }
         }
-        printf("Out of loop");
         swapPos++;
         swap((pArray + swapPos), (pArray + i));
         if(start < swapPos - 1){
@@ -103,8 +120,86 @@ void quickSort(int * pArray, int start, int end){
     return;
 }
 
-void mergeSort(int * pArray, int start, int end){
+/*
+mergeSort
+main mergesort functions that creates the temp array and passes
+args to the helper
+*/
+void mergeSort(int * pArray, int length){
+
+    int * pTempArray;
+    pTempArray = (int *) malloc(length * sizeof(int));
+
+    mergeSortHelp(pArray, pTempArray, 0, length-1);
+    return;
+}
+
+/*
+mergeSortHelp
+recursively calls itself on its left and right halves and then calls merge
+on both of them
+*/
+void mergeSortHelp(int * pArray, int * pTempArray, int start, int end){
+    if(start < end){
+        int mid = (start+end)/2;
+
+        mergeSortHelp(pArray, pTempArray, start, mid);
+        mergeSortHelp(pArray, pTempArray, mid + 1, end);
+
+        merge(pArray, pTempArray, start, mid, end);
+    }
+
+    return;
+}
+
+/*
+merge
+merges two sorted list into one complete sorted list
+*/
+void merge(int * pArray, int * pTempArray, int leftStart, int mid, int rightEnd){
+    int size = rightEnd - leftStart + 1;
+
+    int lIndex = leftStart;
+    int rIndex = mid + 1;
+    int index = leftStart;
+
+    while(lIndex <= mid && rIndex <= rightEnd){
+        if(*(pArray + lIndex) <= *(pArray + rIndex)){
+            *(pTempArray + index) = *(pArray + lIndex);
+            lIndex++;
+        } else {
+            *(pTempArray + index) = *(pArray + rIndex);
+            rIndex++;
+        }
+        index++;
+    }
+
+    while(lIndex <= mid){
+        *(pTempArray + index) = *(pArray + lIndex);
+        lIndex++;
+        index++;
+    }
+
+    while(rIndex <= rightEnd){
+        *(pTempArray + index) = *(pArray + rIndex);
+        rIndex++;
+        index++;
+    }
+
+    index = leftStart;
+    while(index <= rightEnd){
+        *(pArray + index) = *(pTempArray + index);
+        index++;
+    }
+
+    return;
 
 }
 
-
+void swap(int *x, int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    return;
+}
